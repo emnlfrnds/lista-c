a13e6d6e2ce84fdcfd27cdaf3e8440ca2485b6bb
emnlfrnds <enzod2716@gmail.com>
2026-05-30 23:01:31 -0300

    Resolução da questão 7.

--- questao7.c
+++ questao7.c
@@ -19,15 +19,48 @@ int main(void)
 
     int matriz[N][N];
     int valor = 1;
-    int ultimo_valor = N * N;
+
+    int linha_inicial = 0;
+    int linha_final = N - 1;
+    int coluna_inicial = 0;
+    int coluna_final = N - 1;
+
+    while (valor <= N * N)
+    {
+        for (int i = coluna_inicial; i <= coluna_final; i++)
+        {
+            matriz[linha_inicial][i] = valor;
+            valor++;
+        }
+        linha_inicial++;
+
+        for (int i = linha_inicial; i <= linha_final; i++)
+        {
+            matriz[i][coluna_final] = valor;
+            valor++;
+        }
+        coluna_final--;
+
+        for (int i = coluna_final; i >= coluna_inicial; i--)
+        {
+            matriz[linha_final][i] = valor;
+            valor++;
+        }
+        linha_final--;
+
+        for (int i = linha_final; i >= linha_inicial; i--)
+        {
+            matriz[i][coluna_inicial] = valor;
+            valor++;
+        }
+        coluna_inicial++;
+    }
 
     for (int i = 0; i < N; i++)
     {
-        for (int j = N - 1; j >= 0; j--)
+        for (int j = 0; j < N; j++)
         {
-            matriz[i][j] = valor;
-            valor++;
-            printf("%d ", matriz[i][j]);
+            printf("%3d ", matriz[i][j]);
         }
         printf("\n");
     }
